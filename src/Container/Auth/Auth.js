e242a3a179a66251a0ef610605071c53df491ad0
Manuel Gallego <manuel.gallego@ehidra.com>
2018-04-10 10:37:10 +0200

    Wrong authentication message.

--- src/Container/Auth/Auth.js
+++ src/Container/Auth/Auth.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Glyphicon } from 'react-bootstrap';
+import { Glyphicon, Alert } from 'react-bootstrap';
 import './Auth.css';
 import { Redirect } from 'react-router-dom';
 
 class AuthPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {email: '', password:'', logged: false, checked: false, mounted: false};
+    this.state = {email: '', password:'', logged: false, checked: false, mounted: false, message: {txt: "", type: ""}};
     this.handleLogin = this.handleLogin.bind(this);
     this.handleChangeEmail = this.handleChangeEmail.bind(this);
     this.handleChangePassword = this.handleChangePassword.bind(this);
@@ -21,15 +21,17 @@ class AuthPage extends Component {
   }
 
   handleLogin() {
-    this.props.fb.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-    .then(res => {
-      if(res){
-        res.getToken().then(token => console.log(token));
-        this.setState({logged: true});
-      }
-    })
-    .catch(ex => {
-      console.log(ex)
+    this.setState({message: {txt: "Comprobando...", type: "info"}}, () => {
+      this.props.fb.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(res => {
+        if(res){
+          //res.getToken().then(token => console.log(token));
+          this.setState({logged: true, message: {txt: "", type: ""}});
+        }
+      })
+      .catch(ex => {
+        this.setState({message: {txt: "Error de usuario, compruebe su usuario y contraseña", type: "danger"}});
+      });
     });
   }
 
@@ -50,12 +52,13 @@ class AuthPage extends Component {
               (
                 <div className="col-xs-12 col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
                   <div className="login-title">Utr <i className="fas fa-bolt"></i> Ekg</div>
+                  {(this.state.message.txt !== "") && (<Alert bsStyle={this.state.message.type}>{this.state.message.txt}</Alert>)}
                   <div className="input-group">
-                    <span className="input-group-addon"><i className="glyphicon glyphicon-envelope"></i></span>
+                    <span className="input-group-addon"><Glyphicon glyph="envelope" /></span>
                     <input type="email" value={this.state.email} onChange={this.handleChangeEmail} className="form-control"/>
                   </div>
                   <div className="input-group" id="input-group-password">
-                    <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
+                    <span className="input-group-addon"><Glyphicon glyph="lock" /></span>
                     <input type="password" value={this.state.password} onChange={this.handleChangePassword} className="form-control" />
                   </div>
                   <div className="text-center"> 
